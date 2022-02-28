10001e072d66e0b76c75b2672f039e8b57a7160d
Rinat <titanic815@mail.ru>
2022-02-28 16:21:03 +0300

    HW7

---
+++ lesson7/src/main/java/org/example/Lesson06/AutorPage.java
@@ -0,0 +1,18 @@
+package org.example.Lesson06;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AutorPage extends BasePage {
+    public AutorPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @Step("check mark")
+    public AutorPage checkBookMark(){
+        return this;
+    }
+
+}
