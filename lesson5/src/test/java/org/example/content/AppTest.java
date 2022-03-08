d2c431e1bd28ca4aa6b2f6f8566bad9d918c183c
Rinat <titanic815@mail.ru>
2022-03-08 19:58:13 +0300

    HW_5

---
+++ lesson5/src/test/java/org/example/content/AppTest.java
@@ -0,0 +1,16 @@
+package org.example.content;
+
+//import static org.junit.Assert.assertTrue;
+
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void myTest() {
+        System.out.println("myTest");
+    }
+
+}
